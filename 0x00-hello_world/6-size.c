1407082ca1ffe5a7eef159d1e6a4b5233d8ea8e9
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-05 18:27:09 -0700

    intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - This describes the main fuction code
- * Return: 0 
+ *main-This describes the main fuction code
+ *Return:0 
  */
 int main(void)
 {
@@ -11,10 +11,10 @@ long int y;
 long long int c;
 float x;
 
-printf("Size of a char: %lubyte(s)\n", (unsigned long)sizeof(z));
-printf("Size of an int: %lubyte(s)\n", (unsigned long)sizeof(w));
-printf("Size of a long int: %lubyte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a long long int: %lubyte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %lubyte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a char:%lubyte(s)\n", (unsigned long)sizeof(z));
+printf("Size of an int:%lubyte(s)\n", (unsigned long)sizeof(w));
+printf("Size of a long int:%lubyte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a long long int:%lubyte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float:%lubyte(s)\n", (unsigned long)sizeof(x));
 return (0);
 }

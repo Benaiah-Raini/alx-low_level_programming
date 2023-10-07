ad3679ba8a77f99eac4a74bf418414479014e7bc
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-06 17:08:25 -0700

    put

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function printf
+ * Return: 0
+ */
+int main(void)
+{ /* declare a variable and initiate it */
+char alpha = 'a';
+/* while loop */
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+return (0);
+}

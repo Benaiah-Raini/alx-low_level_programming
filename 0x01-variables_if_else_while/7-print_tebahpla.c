9d379d718ae03dbe1170940fbb3d1defd34244be
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-06 18:10:25 -0700

    chom

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function printf
+ * Return: 0
+ */
+int main(void)
+{
+char bena;
+for (bena = 'z'; bena >= 'a'; bena--)
+{
+putchar(bena);
+}
+putchar('\n');
+return (0);
+}

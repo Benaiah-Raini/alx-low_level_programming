e0307c74e33319eb3db08e29f06978b374bb190e
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-06 17:21:12 -0700

    alphaa

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function printf
+ * Return: 0
+ */
+int main(void)
+{
+char b;
+/* for loop*/
+for (b = 'a'; b <= 'z'; b++)
+putchar(b);
+putchar('\n');
+return (0);
+}

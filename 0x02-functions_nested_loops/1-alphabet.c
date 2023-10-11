ae3451848db432ada9815c44f1f8a605ab544b45
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-11 16:39:35 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the function that checks the codes
+ *
+ * Return: Always 0
+ */
+/* prototype function print_alphabet */
+void print_alphabet(void)
+{
+char b;
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}

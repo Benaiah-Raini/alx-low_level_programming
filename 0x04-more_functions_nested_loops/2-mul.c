40cc6681933430b5381bde929fd4fbdf922b9271
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-12 13:25:04 -0700

    man

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - this is the function that adds two digits
+ * @a: The character to be multiplied
+ * @b: The character to be multiplied
+ * Return: the multiplication of two digits
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}

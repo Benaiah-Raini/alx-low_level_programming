6e58fd7ae9293d5d2ee94110843ee20350ec405a
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-12 13:40:20 -0700

    put

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - This function prints all natural numbers from n to 98
+ *void : the starting number
+ *Return: 0.
+ */
+void print_numbers(void)
+{
+int i;
+while (i <= 9)
+{
+_putchar('i');
+_putchar('\n');
+1++
+}
+return (0);
+}

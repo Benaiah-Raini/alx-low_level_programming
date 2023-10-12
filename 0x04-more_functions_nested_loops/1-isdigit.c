fd91d9e813d96c2d63f8a30d0e9eab1c4a6a4304
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-12 13:19:04 -0700

    amen

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - this is the function that checks the codes
+ * @c: The character to be checked
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 /* Prototypes for your functions */
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 #endif /* MAIN_H */

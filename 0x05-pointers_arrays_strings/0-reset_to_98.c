91109d24f9d829a984b2ad2ba3c6c79770e218ad
Benaiah-Raini <benaiahraini@gmail.com>
2023-10-16 14:49:23 -0700

    MAN

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - This the function that takes a pointer to an int and updates
+ * 98 - the value it points to
+ * @n: Integer input pointer
+ * Return - 0:
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Prototypes for your functions */
+void reset_to_98(int *n);
+#endif /* MAIN_H */
